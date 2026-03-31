75d2b05ab4ecb54eaf0cb808b1382021762767cf
mcidf12 <mcid653@gmail.com>
2026-03-31 17:54:18 -0600

    limpiar componente crear cuenta

--- src/app/auth/create-account/create-account.ts
+++ src/app/auth/create-account/create-account.ts
@@ -26,9 +26,7 @@ export class CreateAccount {
   constructor(private fb: FormBuilder, private router: Router, private api: LoginS) {
     this.createForm = this.fb.group({
       numero_cliente: ['', [Validators.required, Validators.maxLength(6), Validators.pattern('^[0-9]+$')]],
-      //password: ['', [Validators.required, Validators.minLength(8)]],
-      //password_confirmation: ['', [Validators.required, Validators.minLength(8)]],
-    }, //{ validators: this.passwordMatchValidator }
+    }, 
     );
   }
 
@@ -46,10 +44,6 @@ export class CreateAccount {
 
 
   get numero_cliente() { return this.createForm.controls['numero_cliente']; }
-  //get password() { return this.createForm.controls['password']; }
-  //get passwordConfirmation() { return this.createForm.controls['password_confirmation']; }
-
-
 
   register() {
     if (this.loading) return;
@@ -65,9 +59,6 @@ export class CreateAccount {
 
     const payload1: any = {
       numero_cliente: codigoNormalizado,
-      //email: raw.email,
-      //password: raw.password,
-      //password_confirmation: raw.password_confirmation
     };
 
     this.api.register(payload1 as any).subscribe({
@@ -87,8 +78,6 @@ export class CreateAccount {
         } else if (e?.status === 409) {
           toast.error('Este cliente ya tiene una cuenta registrada')
         } else if (e?.status === 422) {
-          toast.error('Este correo ya tiene una cuenta registrada')
-        } else if (e?.status === 403) {
           toast.error('Este numero de cliente ha sido dado de baja')
         } else if (e?.status === 404) {
           toast.error('Cliente no encontrado')
