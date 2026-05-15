13f13755fb52ccc28d50210b5b0d71820d6a58bb
mcidf12 <mcid653@gmail.com>
2026-05-14 18:07:53 -0600

    visitas

--- src/app/pages/visits/visits.ts
+++ src/app/pages/visits/visits.ts
@@ -1,11 +1,46 @@
+import { NgClass, NgIf, NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-visits',
-  imports: [],
+  imports: [NgClass, NgIf, NgForOf],
   templateUrl: './visits.html',
   styleUrl: './visits.css'
 })
 export class Visits {
+
+  visitas = [
+    {
+      asunto: 'Sin conexión a Internet',
+      fecha: '13 Mayo 2026',
+      tecnico: 'Luis Bernal',
+      estatus: 'atendido',
+      detalles: 'Se realizó revisión de ONT y reconexión.',
+      diagnostico: 'Sin conexión a Internet',
+      solucion: 'Se restableció el acceso a Internet'
+    },
+    {
+      asunto: 'Foco Rojo',
+      fecha: '10 Mayo 2026',
+      tecnico: 'Daniel',
+      estatus: 'cancelado',
+      detalles: 'Cliente no se encontraba en domicilio.',
+      diagnostico: 'Sin conexión a Internet',
+      solucion: 'Se restableció el acceso a Internet'
+    },
+    {
+      asunto: 'Internet lento a',
+      fecha: '3 Mayo 2026',
+      tecnico: 'Daniel',
+      estatus: 'pendiente',
+      detalles: 'Cliente no se encontraba en domicilio.'
+    }
+  ];
+
+  visitaSeleccionada: any = null;
+
+  abrirDetalle(visita: any) {
+    this.visitaSeleccionada = visita;
+  }
 
 }
