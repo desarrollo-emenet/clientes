d69924579ea7df734ffac29861b981b368ca0709
mcidf12 <mcid653@gmail.com>
2026-07-11 14:03:49 -0600

    config: environmet para produccion

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiLocalUrl: 'http://localhost:8085/api',  //apunta al backend
+  apiLocalUrl2: 'https://isp-back.emenet.mx/api', //apunta a pago-bancoV
+  tokenKey: 'wDApSmjJxmYA9Tf2fgSV15gfuYLe5au4gCZA7zGwegE25IVLWeWFo9dCUugVrGYY'
+  
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiLocalUrl: 'http://192.168.18.24:8085/api', 
+  apiLocalUrl2: 'https://isp-back.emenet.mx/api', //apunta a pago-bancoV2
+  tokenKey: 'wDApSmjJxmYA9Tf2fgSV15gfuYLe5au4gCZA7zGwegE25IVLWeWFo9dCUugVrGYY'
+  
+};
