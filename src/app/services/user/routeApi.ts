65ff244473ba53e1c05b9fc17fd2e01a7595491b
mcidf12 <mcid653@gmail.com>
2026-07-11 12:19:41 -0600

    configuracion angular json

--- src/app/services/user/routeApi.ts
+++ src/app/services/user/routeApi.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiLocalUrl: 'http://localhost:8000/api',  //apunta al backend
+  apiLocalUrl: 'http://localhost:8085/api',  //apunta al backend
   //apiLocalUrl: 'http://192.168.110.101:8000/api', 
   apiLocalUrl2: 'https://isp-back.emenet.mx/api', //apunta a pago-bancoV2
 
