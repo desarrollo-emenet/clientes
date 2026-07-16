8c7946f54bd526a4b2724ee716b4536e0200bb11
mcidf12 <mcid653@gmail.com>
2026-07-16 11:38:58 -0600

    filereplacements en angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiLocalUrl: 'http://192.168.18.24:8085/api',  //apunta al backend
+  apiLocalUrl: 'http://192.168.110.24:8085/api',  //apunta al backend
   apiLocalUrl2: 'https://isp-back.emenet.mx/api', //apunta a pago-bancoV
   tokenKey: 'wDApSmjJxmYA9Tf2fgSV15gfuYLe5au4gCZA7zGwegE25IVLWeWFo9dCUugVrGYY'
   
