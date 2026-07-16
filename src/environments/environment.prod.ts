47ace994bdf05d42bb0cfd59509f36c861f41bc0
mcidf12 <mcid653@gmail.com>
2026-07-16 12:16:13 -0600

    environments

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiLocalUrl: 'http://192.168.110.24:8085/api',  //apunta al backend
-  apiLocalUrl2: 'https://isp-back.emenet.mx/api', //apunta a pago-bancoV
-  tokenKey: 'wDApSmjJxmYA9Tf2fgSV15gfuYLe5au4gCZA7zGwegE25IVLWeWFo9dCUugVrGYY'
-  
-};
