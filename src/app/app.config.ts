a9d3264d01b2cf98630ff74d74722dad97e6ec17
mcidf12 <mcid653@gmail.com>
2026-07-28 17:59:47 -0600

    scroll fijo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { provideHttpClient, /*, withInterceptors*/ 
-withInterceptors} from '@angular/common/http';
+import {
+  provideHttpClient, /*, withInterceptors*/
+  withInterceptors
+} from '@angular/common/http';
 import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { authInterceptor } from './guards/auth.interceptor';
 
@@ -14,7 +16,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'top',
+      anchorScrolling: 'enabled'
+    })),
     provideNativeDateAdapter(),
     provideHttpClient(withInterceptors([authInterceptor])),
     { provide: MAT_DATE_LOCALE, useValue: 'es-MX' }
