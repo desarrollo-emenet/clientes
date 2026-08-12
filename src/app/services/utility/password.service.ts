6c8f8e7378d84079a50c703abe53445937f35c05
Cesar Rivera <cesarivan25479@gmail.com>
2026-08-12 17:26:11 -0600

    correccion componentes recuperacion

---
+++ src/app/services/utility/password.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PasswordService {
+  matchValidator(group: FormGroup) {
+    const pass = group.get('password')?.value;
+    const confirm = group.get('password_confirmation')?.value;
+    if (pass && confirm && pass !== confirm) {
+      return { passwordMissMatch: true };
+    }
+    return null;
+  }
+
+  calculateStrength(password: string): number {
+    if (!password) {
+      return 0;
+    }
+    let strength = 0;
+    if (password.length >= 8) strength++;
+    if (/\d/.test(password)) strength++;
+    if (/[A-Z]/.test(password) && /[a-z]/.test(password)) strength++;
+    if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) strength++;
+
+    return strength;
+  }
+
+  public feddback(passwordStrength: number): any{
+    switch (passwordStrength) {
+      case 1:
+        return { class: 'strength-weak', text: 'Débil' }
+      case 2:
+        return { class: 'strength-medium', text: 'Moderada' }
+      case 3:
+        return { class: 'strength-good', text: 'Buena' }
+      case 4:
+        return { class: 'strength-strong', text: 'Fuerte' }
+      default:
+        return { class: 'strength-none', text: 'Muy débil' }
+    }
+  }
+}
